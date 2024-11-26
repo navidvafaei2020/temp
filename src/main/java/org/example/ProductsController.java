6d18739a2f88b100cb2d5152285e85ccd6a4c114
Navid Vafaei <navidvafaei2020@gmail.com>
2024-11-26 14:36:17 +0100

    Initial commit

---
+++ src/main/java/org/example/ProductsController.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @GetMapping
+    public String getProduct(){
+        return "Http get handled";
+    }
+
+    @PostMapping
+    public String createNewProduct(){
+        return "Http post handled";
+    }
+
+    @PutMapping
+    public String editProduct(){
+        return "Http Put handled";
+    }
+
+    @DeleteMapping
+    public String deleteMapping(){
+        return "Http Delete hanfled";
+    }
+
+}
